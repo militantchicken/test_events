bb5b5b25ec6f8acf9187845ee1507772218bb43a
Prelang Builder <builder@prelang.com>
2016-01-13 00:19:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Alert' to allow for create, update

--- app/admin/alert.rb
+++ app/admin/alert.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Alert do
+  permit_params :alert_time
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
