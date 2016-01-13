9f6698d04b12004e6d4e7ae961140b22767af41e
Prelang Builder <builder@prelang.com>
2016-01-13 00:18:57 +0000

    Installed relation 'has_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   #->Prelang (user_login:devise/username_login_support)
   has_many :comments
   has_many :alerts
+  has_many :user_subs
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
